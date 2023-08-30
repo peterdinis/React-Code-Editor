eaedd7d11ea9fd86c4d3db3c5e41da26bdef633e
Peterdinis <peterdinis0@pobox.sk>
2023-08-30 17:29:02 +0200

    infra: Removing packages that are not used in applications

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/css/css';
 import {Toaster} from "react-hot-toast";
 import { RecoilRoot } from "recoil";
 

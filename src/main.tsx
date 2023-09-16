27b777713dd3afa7caec96c5ec8e3136f1cdbb05
Peterdinis <peterdinis0@pobox.sk>
2023-09-16 16:23:56 +0200

    FOO

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
